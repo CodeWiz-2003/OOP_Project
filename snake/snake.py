802d1dc3dea11eee67c97b143ae19f280efcf371
Abdul Amaan <110830860+Amaan1234567@users.noreply.github.com>
2024-02-05 10:39:17 +0530

    small fix

--- snake/snake.py
+++ snake/snake.py
@@ -8,17 +8,17 @@ class snake:
     -----------
     __screen : pygame.Surface
         The main screen surface where the snake is displayed.
-    __blockSize : int
+    __block_size : int
         The size of each block of the snake.
     positions : list
         A list of positions of the blocks of the snake. Each position is represented as a list of two integers.
     direction : str
         The current direction of the snake. It can be 'right', 'left', 'up', or 'down'.
     """
-    def __init__(self, screen, blockSize):
+    def __init__(self, screen, block_size):
 
         self.__screen = screen
-        self.__blockSize = blockSize
+        self.__block_size = block_size
         self.positions = [[500,500],[490,500],[480,500]]
         self.direction = 'right'
 
@@ -29,13 +29,13 @@ class snake:
         """
         head = self.positions[0].copy()
         if self.direction == 'right':
-            head[0] += self.__blockSize
+            head[0] += self.__block_size
         if self.direction == 'left':
-            head[0] -= self.__blockSize
+            head[0] -= self.__block_size
         if self.direction == 'up':
-            head[1] -= self.__blockSize
+            head[1] -= self.__block_size
         if self.direction == 'down':
-            head[1] += self.__blockSize
+            head[1] += self.__block_size
         for position in self.positions[:-1]:
             if head[0] == position[0] and head[1] == position[1]:
                 return False
@@ -44,7 +44,7 @@ class snake:
         self.positions.pop()
         return True
 
-    def addBlock(self, position):
+    def add_block(self, position):
         """
         Adds a new block to the snake at the given position.
         """
@@ -55,4 +55,4 @@ class snake:
         Draws the snake on the screen.
         """
         for position in self.positions:
-            pygame.draw.rect(self.__screen, (0,255,0), (*position, self.__blockSize, self.__blockSize))
+            pygame.draw.rect(self.__screen, (0,255,0), (*position, self.__block_size, self.__block_size))
